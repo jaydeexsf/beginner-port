6107575de45d2170dd1b3128cb4b22453e430f0a
jaydeexsf <mojohannes06@gmail.com>
2024-02-15 15:35:18 -0800

    last commit

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+// const menuAppear = document.getElementsByName('menu');
+// const appearing = document.querySelector('.right-home-1');
 
+// menuAppear.addEventListener('onclick', () => {
+//     alert('you clicked');
+//     console.log('yeah')
+// });
 
+// console.log('whyjgh');
+
+// let slide = document.querySelector(".all-p");
+// function mt () {
+// setInterval(() => {
+//    slide.style.translatex = '-50%'; 
+// }, 2000);
+
+// }
+// mt()
